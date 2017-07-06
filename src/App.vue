1514fad682f83398b5dceebc4c1a5e386fc01a36
Chai Phonbopit <phonbopit@gmail.com>
2017-07-07 00:48:56 +0700

    update component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <SuperHeader></SuperHeader>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import SuperHeader from './components/Header'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    SuperHeader
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+	<header class="header">
+		<section class="hero is-success">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">{{ title }}</h1>
+					<h2>{{ subtitle }}</h2>
+				</div>
+			</div>
+		</section>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: 'super-header',
+		data() {
+			return {
+				title: 'Vue Chat',
+				subtitle: 'Simple Chat Application with Vue.js'
+			}
+		}
+	}
+</script>
